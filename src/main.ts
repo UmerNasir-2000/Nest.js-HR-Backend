05013aaf69d29b5258759d81a159ea40f30dba9c
Umer Nasir <umer.nas@outlook.com>
2022-03-03 23:55:46 +0500

    Removed Extra Line

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  console.log(process.env.PASSWORD);
   await app.listen(process.env.HTTP_PORT);
 }
 bootstrap();
